641748e8b92e49ff5f32f722d03eca2436bd7f79
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-29 18:33:11 -0300

    QUESTÃO8.PY
    RESOLUÇÃO DA EXEMPLO 8 DA PRIMEIRA LISTA DE ALGORITIMO E PROGAMAÇÃO.

--- QUESTÃO6.py
+++ QUESTÃO6.py
@@ -2,5 +2,4 @@ velocidade_kmh = float(input("Digite a velocidade em km/h: "))
 
 
 velocidade_ms = velocidade_kmh / 3.6
-
 print(f"A velocidade equivalente é de {velocidade_ms: .2f} m/s.")
---
+++ QUESTÃO8.py
@@ -0,0 +1,10 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+subtracao = num1 - num2
+
+resuldado_final = soma/subtracao
+
+print(f"O resultado da divisão da soma pela subtração é: {resultado_final: .2f}")
