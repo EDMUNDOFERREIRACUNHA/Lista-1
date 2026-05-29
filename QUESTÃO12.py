bd9c2e6c9c2e9c2e3663554b1b49df46912a74c1
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-29 19:28:04 -0300

    QUESTÃO12.PY
    RESOLUÇÃO DA QUESTÃO 12 DA LISTA 1 DE ALGORITIMO E PROGRAMAÇÃO.

---
+++ QUESTÃO12.py
@@ -0,0 +1,5 @@
+salario_atual = float(input("Digite o salário atual do trabalhador: R$ "))
+
+salario_novo = salario_atual * 1.25
+
+print(f"O novo salário com 25% de aumento é: R$ { salario_novo: .2f}")
