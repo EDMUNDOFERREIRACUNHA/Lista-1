3c5632e7f52e6259b311111b94ded53c6c924d1b
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-29 18:12:59 -0300

    QUESTÃO7.PY
    Resolução do exemplo 7 da primeira lista de algoritimo e programação

--- QUESTÃO6.py
+++ QUESTÃO6.py
@@ -3,4 +3,4 @@ velocidade_kmh = float(input("Digite a velocidade em km/h: "))
 
 velocidade_ms = velocidade_kmh / 3.6
 
-print(f"A velocidade equivalente é de {velocidade_ms: .2f} m/s.")458
+print(f"A velocidade equivalente é de {velocidade_ms: .2f} m/s.")
---
+++ QUESTÃO7.py
@@ -0,0 +1,12 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+
+soma_primeiros = num1 + num2
+
+diferenca_ultimos = num2 - num3
+
+
+print(f"A soma dos dois primeiros números é: {soma_primeiros}")
+print(f"A diferença entre os dois últimos números é: {diferenca_ultimos}")
