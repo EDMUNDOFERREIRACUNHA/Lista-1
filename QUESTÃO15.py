2af35c8278f4f91a2adc4dab930bc6f7d5c47db2
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-31 08:55:02 -0300

    QUESTÃO15.PY
    RESOLUÇÃO DA QUESTÃO 15 DA PRIMEIRA LISTA DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO15.py
@@ -0,0 +1,6 @@
+base = float(input("Digite o valor da base do triangulo: "))
+altura = float(input("Digite o valor da altura do triângulo: "))
+
+area = (base * altura) / 2
+
+print(f"A área do triangulo é: {areaq: .2f}")
