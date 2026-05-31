11849c334e9c307759eda318adac910387f92995
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-31 09:52:03 -0300

    QUESTÃO20.PY
    RESOLUÇÃO DA QUESTÃO 20 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO20.py
@@ -0,0 +1,5 @@
+temperatura_c = float(input(" Digite a temperatura em graus Celsius (°C): "))
+
+temperatura_f = ((9 * temperatura_c) + 160) / 5
+
+print(f"A temperatura equivalente é: {temperatura_f: .1f}°F")
