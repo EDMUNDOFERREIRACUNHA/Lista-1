e11d9777b2351678ac5e84e73c32647609648f2d
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-31 09:59:20 -0300

    QUESTÃO21.PY
    RESOLUÇÃO DA QUESTÃO 21 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO21.py
@@ -0,0 +1,5 @@
+temperatura_f = float(input("Digite a temperatura em graus Fahrenheit (°f): "))
+
+temperatura_c = ((5 * temperatura_f) - 160) / 9
+
+print(f"A temperatura equivalente é: {temperatura_c: .1f}°F")
