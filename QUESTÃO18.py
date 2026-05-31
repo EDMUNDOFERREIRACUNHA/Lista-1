aeda630e23af8b3e73da219095aa2e5720a1a2f8
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-05-31 09:33:58 -0300

    QUESTÃO18.PY
    RESOLUÇÃO DA QUESTÃO 18 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO18.py
@@ -0,0 +1,7 @@
+import math
+
+raio = float(input("Digite o valor do raio da circunferência: "))
+
+comprimento = 2 * math.pi * raio
+
+print(f"O comprimento da circunferência é: {comprimento: .2f}")
