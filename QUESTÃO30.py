43727d3af8d8f87b439ded9a68a1024ee1b74599
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-02 12:00:52 -0300

    QUESTÃO30.PY
    RESOLUÇÃO DA QUESTÃO 30 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO30.py
@@ -0,0 +1,10 @@
+minutos_total = int(input("Digite a quantidade total de minutos(inteiro): "))
+
+dias = minutos_total // 1440
+
+resto_minutos = minutos_total % 1440
+
+horas = resto_minutos % 60
+
+print(f"{minutos_total} minutos correspondem a; ")
+print({dias} dia(s, {horas hora(s e {minutos_finai} minuto(s))}))
