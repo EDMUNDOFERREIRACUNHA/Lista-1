6488d57c9d61d6f513ccb2d15fb1865409e80e47
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-02 11:51:44 -0300

    QUESTÃO29.PY
    RESOLUÇÃO DA QUESTÃO 29 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO29.py
@@ -0,0 +1,7 @@
+meses_total = int(input("Digite a quantidade total de meses(inteiro): "))
+
+anos = meses_total // 12
+
+meses_restantes = meses_total % 12
+ 
+ print(f" {meses_total} meses correspondem a: {anos ano(s e {meses_restantes} mês(es))}")
