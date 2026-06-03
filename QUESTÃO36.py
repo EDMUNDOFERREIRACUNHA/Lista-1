090aa3bace6288fb5cb4e1305bf13edb55f097ec
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-03 12:01:33 -0300

    QUESTÃO36.PY
    RESOLUÇÃO DA QUESTÃO 36 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO36.py
@@ -0,0 +1,7 @@
+anos = int(input("Digite a quantidade de anos: "))
+meses = int(input("Digite a quantidade de meses: "))
+dias = int(input("Digite a quantidade de dias: "))
+
+idade_em_dias = (anos * 365) + (meses * 30) + dias
+
+print(f"A idade totalnexpressa apenas em dias é: {idade_em_dias} dias.")
