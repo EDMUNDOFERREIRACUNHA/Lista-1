b5a0ab63dd647fd66bab4a385d92bc2a8970459d
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-03 10:22:25 -0300

    QUESTÃO31.PY
    RESOLUÇÃO DA QUESTÃO 31 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO31.py
@@ -0,0 +1,5 @@
+binario_texto = input("Digite um número binário de 4 dígitos (ex: 1011): ")
+
+decimal = int(binario_texto, 2)
+
+print(f"O número binário {binario_texto} na base decimal é {decimal}")
