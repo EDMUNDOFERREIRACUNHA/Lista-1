1df9110cbd8ff0960ff907664061ce4ac474c527
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-04 12:27:41 -0300

    QUESTÃO39.PY
    RESOLUÇÃO DA QUESTÃO 39 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO39.py
@@ -0,0 +1,17 @@
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+C = int(input("Digite o valor de C: "))
+
+
+R = (A + B) ** 2
+
+S = (B + C) ** 2
+
+D = (R + S) / 2
+
+
+
+print("\n--- RESULTADOS ---")
+print(f"Valor de R: {R}")
+print(f" Valor de S: {S}")
+print(f"O valor da expressão D é: {D: .2f}")
