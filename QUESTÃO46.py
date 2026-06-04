257883ab1bf391c4238cc358518641feccc1854b
EDMUNDOFERREIRACUNHA <edmundoferreiracunha@gmail.com>
2026-06-04 14:09:10 -0300

    QUESTÃO46.PY
    RESOLUÇÃO DA QUESTÃO 46 DA LISTA 1 DE ALGORITMO E PROGRAMAÇÃO.

---
+++ QUESTÃO46.py
@@ -0,0 +1,11 @@
+celsius = float(input("Digite a temperatura em graus Celsius(°C): "))
+
+
+
+fahrenheit = (celsius * 1.8) + 32
+
+
+
+print("\n--- CONVERSÃO DE TEMPERATURA ---")
+print(f"Temperatura original: {celsius: .1f}°C")
+print(f"Temperatura convertida: {fahrenheit: .1f}°F")
